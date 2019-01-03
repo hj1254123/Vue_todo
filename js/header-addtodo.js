1c729426f848907e966fa34d39a1e01fe661fd47
HouJian <hj1254123@qq.com>
2019-01-03 21:52:07 +0800

    拆分JS文件

---
+++ js/header-addtodo.js
@@ -0,0 +1,22 @@
+// 添加任务栏
+Vue.component('header-addtodo', {
+	props: ['value'],
+	data: function() {
+		return {
+
+		}
+	},
+	template: `
+		<header class="header">
+			<h1>todos</h1>
+			<input
+				class="new-todo"
+				placeholder="做的什么？('Enter'添加)"
+				autofocus
+				:value="value"
+				@input="$emit('input', $event.target.value)"
+				@keydown.enter="$emit('addtodo')"
+			>
+		</header>
+		`
+})
